3ac202b5873a84bb6e3410618adab3137a417537
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2017-05-01 23:28:43 +0700

    Implemented client part
    Not tested

---
+++ KeyTransmitter/KeyTransmitter.Client/Transmitter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyTransmitter.Client
+{
+    public class Transmitter : IDisposable
+    {
+        private TcpClient client;
+        private Stream tcpStream;
+
+        public Transmitter(string host, int port)
+        {
+            client = new TcpClient(host, port);
+            tcpStream = client.GetStream();
+        }
+
+        public void SendKey(ConsoleKey key)
+        {
+            tcpStream.Write(BitConverter.GetBytes((int)key), 0, sizeof(int));
+        }
+
+        public async Task SendKeyAsync(ConsoleKey key)
+        {
+            await tcpStream.WriteAsync(BitConverter.GetBytes((int)key), 0, sizeof(int));
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
---
+++ KeyTransmitter/KeyTransmitter.ConsoleClient/Launcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using KeyTransmitter.Client;
+
+namespace KeyTransmitter.ConsoleClient
+{
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Two arguments required: hostname and port");
+                return;
+            }
+
+            try
+            {
+                var hostname = args[0];
+                var port = int.Parse(args[1]);
+
+                using (var transmitter = new Transmitter(hostname, port))
+                {
+                    var key = Console.ReadKey();
+                    while (key.Key != ConsoleKey.Escape)
+                    {
+                        transmitter.SendKey(key.Key);
+                        key = Console.ReadKey();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
